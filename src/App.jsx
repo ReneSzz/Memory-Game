b111cd9e941c1bb5015526b636c41992331896f1
ReneSzz <renesuar99@gmail.com>
2023-09-14 15:46:52 -0400

    Figured out how to access data from async function

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+ function App() {
+ let [spriteBack, setSpriteBack] = useState('');
+ let [name, setName] = useState('');
+ 
+
+  async function getPokemon()
+  {
+    
+     try{
+     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/charizard`);
+     let result = await response.json();
+     //console.log(result.sprites.back_default);
+      setSpriteBack(result.sprites.front_default);
+      setName(result.forms[0].name);
+
+      console.log(spriteBack);
+      console.log(name);
+
+    
+     }
+     catch
+     {
+        title.innerText = "Error Invalid Location";
+      
+     }
+     
+  }
+  
+getPokemon();
 
   return (
     <>
-     
+     <img src={spriteBack}></img>
+     <p>{name}</p>
     </>
   )
 }
