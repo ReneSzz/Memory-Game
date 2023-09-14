e344ae63ea4d3b5a4d6dd99426d5ace389bb03ad
ReneSzz <renesuar99@gmail.com>
2023-09-14 17:20:39 -0400

    Uses setState and pokemon object.

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,51 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+
+
  function App() {
  let [spriteBack, setSpriteBack] = useState('');
  let [name, setName] = useState('');
+ let [pokemonArray, setPokemonArray] = useState([]);
+ let [pokemon, setPokemon] = useState({spriteBack: '', pokemonName:''});
  
 
+
+
+ 
   async function getPokemon()
   {
     
      try{
-     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/charizard`);
+     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/3`);
      let result = await response.json();
-     //console.log(result.sprites.back_default);
       setSpriteBack(result.sprites.front_default);
       setName(result.forms[0].name);
-
-      console.log(spriteBack);
-      console.log(name);
-
-    
+      setPokemon({spriteBack,name});
      }
      catch
      {
-        title.innerText = "Error Invalid Location";
+        console.log("error");
       
      }
      
   }
   
+
+  function displayArray()
+  {
+    console.log(pokemonArray);
+
+  }
 getPokemon();
 
   return (
     <>
-     <img src={spriteBack}></img>
-     <p>{name}</p>
+    <div className='card'>
+    <img src={pokemonArray[0]}></img>
+    <button onClick={displayArray}></button>
+   
+    </div>
     </>
   )
 }
