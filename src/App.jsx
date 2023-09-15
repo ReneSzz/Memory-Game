48643ffde4b3829b5a4ee3194fab1591fdaf7241
ReneSzz <renesuar99@gmail.com>
2023-09-15 10:32:09 -0400

    Used new api call

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,63 @@ import './App.css'
 
 
  function App() {
+  let [index, setIndex] = useState(0);
+const URL = `https://pokeapi.co/api/v2/pokemon/3`
  let [spriteBack, setSpriteBack] = useState('');
  let [name, setName] = useState('');
  let [pokemonArray, setPokemonArray] = useState([]);
  let [pokemon, setPokemon] = useState({spriteBack: '', pokemonName:''});
+ const [results, setResults] = useState([]);
+ const controller = new AbortController;
  
 
+ function Pokemon({ name,sprite, props }) {
+  return <> 
+  
+<h1 {...props}>{name}</h1>;
+<img src={sprite}></img>
+
+</>
+}
 
 
- 
-  async function getPokemon()
-  {
+
+
+   async function getPokemon()
+   {
+     
+      let response = await fetch(URL);
+      if (response.ok){
+        return await response.json();
+      } else {
+        return Promise.reject();
+      }
     
-     try{
-      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/3`);
-      let result = await response.json();
-      setSpriteBack(result.sprites.front_default);
-      setName(result.forms[0].name);
-      setPokemon({spriteBack,name});
-     // console.log(pokemonArray);
-      
-   
-     }
-     catch {console.log("error");}
-    
-      
   }
 
-
-
  
+useEffect(() => {
+getPokemon().then(json => setPokemon(json));
+});
 
-
-
-
-getPokemon(); 
+  // setResults(result);
+  // setSpriteBack(result.sprites.front_default);
+  // setName(result.forms[0].name);
+  // setPokemon({spriteBack,name})
 
   return (
     <>
-    <div className='card'>
-    <img src={pokemon.spriteBack}></img>
+    <div>
+      <button type="button" onClick={()=> setIndex(index + 1)}> Next </button>
+
+      {pokemon ? (
+        <Pokemon name={pokemon.name} sprite={pokemon.sprites.front_default} />
+      ) : (
+
+        <div> no pokemon for {index}</div>
+      )}
     </div>
+   
     </>
   )
 }
